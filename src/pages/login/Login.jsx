f03a530856c28b92ef48431cde0d53d8da8ee410
Abdullah Muhammad Siam <abdullahms838@gmail.com>
2023-07-15 18:57:16 +0600

    login form

--- src/pages/login/Login.jsx
+++ src/pages/login/Login.jsx
@@ -1,10 +1,51 @@
 import React from 'react';
+import { Container, Form, Button } from 'react-bootstrap';
+import { Link } from 'react-router-dom';
+
+
 
 const Login = () => {
+    const handleSignIn = event => {
+        event.preventDefault();
+
+        const form = event.target;
+        const email = form.email.value;
+        const password = form.password.value;
+        console.log(email, password);
+
+    }
     return (
-        <div>
-            login page
-        </div>
+        <Container>
+            <h3>Please Login</h3>
+            <Form onSubmit={handleSignIn} className='w-25 mx-auto mt-4'>
+                <Form.Group className="mb-3" controlId="formBasicEmail">
+                    <Form.Label>Enter Your email</Form.Label>
+                    <Form.Control type="email" name='email' placeholder="Enter email" required />
+                </Form.Group>
+
+                <Form.Group className="mb-3" controlId="formBasicPassword">
+                    <Form.Label>Password</Form.Label>
+                    <Form.Control name='password' type="password" placeholder="Password" required />
+                </Form.Group>
+                <Button variant="success" type="submit">
+                    login
+                </Button>
+
+                <br />
+
+                <Form.Text className="text-muted">
+                    Don't have an account <Link to="/register" className='danger'>Register</Link>
+                </Form.Text>
+
+                <Form.Text className="text-muted">
+
+                </Form.Text>
+
+                <Form.Text className="text-muted">
+
+                </Form.Text>
+            </Form>
+        </Container>
     );
 };
 
